ce2c9d257fff8d1b6abc1c6745fbd0a73d6800fd
Ddyadz01 <albertmik2002@gmail.com>
2025-06-01 11:25:11 +0300

    Refactoring

--- src/components/FilmModal/FilmModal.jsx
+++ src/components/FilmModal/FilmModal.jsx
@@ -5,10 +5,12 @@ import { CircleX, Volume2Icon, VolumeOff } from 'lucide-react'
 import { FormatSeconds } from '../../utils/SecondsFormat'
 
 import styles from './film-modal.module.scss'
+import { Loader } from '../Loader/Loader'
 
 export const FilmModal = ({ activeFilm, setSearchParams, setIsModal }) => {
   const [isMuted, setIsMuted] = useState(true)
   const [progress, setProgress] = useState(0)
+  const [isPlaying, setIsPlaying] = useState(false)
   // const [duration, setDuration] = useState(0)
   // const [currentTime, setCurrentTime] = useState('00:00')
 
@@ -31,6 +33,9 @@ export const FilmModal = ({ activeFilm, setSearchParams, setIsModal }) => {
   // }
 
   const timeUpdate = (e) => {
+    if (!videoRef.current.paused) {
+      setIsPlaying(true)
+    }
     setProgress((e.target.currentTime / e.target.duration) * 100)
     if (e.target.currentTime == e.target.duration) {
       videoRef.current.currentTime = 0
@@ -47,60 +52,65 @@ export const FilmModal = ({ activeFilm, setSearchParams, setIsModal }) => {
   }
 
   return (
-    <div className={styles['film--modal']}>
-      <div className={styles['film--modal__content']}>
-        <div className={styles['film--modal__header']}>
-          <CircleX onClick={closeHandler} />
-        </div>
+    <>
+      <title>{activeFilm.title}</title>
+      <div className={styles['film--modal']}>
+        <div className={styles['film--modal__content']}>
+          <div className={styles['film--modal__header']}>
+            <CircleX onClick={closeHandler} />
+          </div>
 
-        <div className={styles['film--modal__body']}>
-          <div className={styles['film--modal__video']}>
-            <video
-              // poster={activeFilm.posterUrl}
-              // onLoadedMetadata={handlerDuration}
-              muted={isMuted}
-              playsinline
-              playsInline={true}
-              ref={videoRef}
-              src={activeFilm.trailerUrl}
-              onTimeUpdate={timeUpdate}
-            ></video>
-            {/* <div className={styles['repeat']}>{!isPlaying && <Repeat />}</div> */}
+          <div className={styles['film--modal__body']}>
+            <div className={styles['film--modal__video']}>
+              <video
+                // poster={activeFilm.posterUrl}
+                // onLoadedMetadata={handlerDuration}
+                style={isPlaying ? {} : { background: 'black' }}
+                muted={isMuted}
+                playsinline
+                playsInline={true}
+                ref={videoRef}
+                src={activeFilm.trailerUrl}
+                onTimeUpdate={timeUpdate}
+              ></video>
+              {/* <div className={styles['repeat']}>{!isPlaying && <Repeat />}</div> */}
+              {!isPlaying && <Loader />}
 
-            <div className={styles['progress']} style={{ width: progress + '%' }}></div>
-            <div className={styles['film--modal__video-footer']}>
-              {/* <p>
+              <div className={styles['progress']} style={{ width: progress + '%' }}></div>
+              <div className={styles['film--modal__video-footer']}>
+                {/* <p>
                 {currentTime} / {duration}
               </p> */}
 
-              {isMuted ? <VolumeOff onClick={volumeOff} /> : <Volume2Icon onClick={volumeOff} />}
+                {isMuted ? <VolumeOff onClick={volumeOff} /> : <Volume2Icon onClick={volumeOff} />}
+              </div>
             </div>
-          </div>
 
-          <div className={styles['film--modal__info']}>
-            <div className={styles['film--modal__title']}>
-              <h1>{activeFilm.title}</h1>
-            </div>
-            <div className={styles['film--modal__more']}>
-              <div className={styles['film--modal__ganres']}>
-                {activeFilm.ganres.map((ganre, idx) => (
-                  <p>
-                    {ganre} {idx + 1 != activeFilm.ganres.length && '∙'}
-                  </p>
-                ))}
+            <div className={styles['film--modal__info']}>
+              <div className={styles['film--modal__title']}>
+                <h1>{activeFilm.title}</h1>
               </div>
-              <div className={styles['film--modal__country']}>
-                {activeFilm.country + ' ∙ ' + activeFilm.year + ' ∙ '}
-                <div className={styles['film--modal__age']}>{activeFilm.minAge + ' +'}</div>
-              </div>
-              <div className={styles['film--modal__description']}>
-                <h1>Описание</h1>
-                {activeFilm.description}
+              <div className={styles['film--modal__more']}>
+                <div className={styles['film--modal__ganres']}>
+                  {activeFilm.ganres.map((ganre, idx) => (
+                    <p>
+                      {ganre} {idx + 1 != activeFilm.ganres.length && '∙'}
+                    </p>
+                  ))}
+                </div>
+                <div className={styles['film--modal__country']}>
+                  {activeFilm.country + ' ∙ ' + activeFilm.year + ' ∙ '}
+                  <div className={styles['film--modal__age']}>{activeFilm.minAge + ' +'}</div>
+                </div>
+                <div className={styles['film--modal__description']}>
+                  <h1>Описание</h1>
+                  {activeFilm.description}
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
